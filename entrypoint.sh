e446e6def583e8c0ce9d27348b88e7809bf04142
faizan35 <faizanansari4862@gmail.com>
2024-04-27 16:37:20 +0530

    config update

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# If running the rails server then create or migrate existing database
+if [ "${*}" == "./bin/rails server" ]; then
+  ./bin/rails db:create
+  ./bin/rails db:prepare
+fi
+
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
